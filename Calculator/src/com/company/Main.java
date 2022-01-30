a7d0357a3d813397a3251e0adb971d386e7c00e6
Rodrigo <rodri45ESP@gmail.com>
2022-01-30 10:54:38 +0100

    code updated
    welcome message added

--- Calculator/src/com/company/Main.java
+++ Calculator/src/com/company/Main.java
@@ -1,9 +1,13 @@
 package com.company;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
+
+@FunctionalInterface
+interface CalculateInterface {
+    void Calculate();
+}
 
 public class Main {
 
@@ -15,30 +19,35 @@ public class Main {
         int option = main.ShowMenu();
 
         if(option == 1) {
+            CalculateInterface sum = () -> {
             Integer result = intList.stream()
                     .mapToInt(Integer::intValue)
                     .sum();
-            System.out.println(result);
+            System.out.println(result);};
+        sum.Calculate();
         }
         else if(option == 2) {
+            CalculateInterface multiplication = () -> {
             int x;
             x = intList.stream().reduce(1, (a, b) -> a * b);
-            System.out.println(x);
+            System.out.println(x);};
+            multiplication.Calculate();
 
 
         }
         else if(option == 3) {
+            CalculateInterface substraction = () -> {
             int x;
             x = intList.stream().reduce(1, (a, b) -> a - b);
-            System.out.println(x);
+            System.out.println(x);};
+            substraction.Calculate();
 
         }
-        System.out.println("finished");
+        System.out.println("Done!");
     }
     public int ShowMenu(){
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to the calculator");
         System.out.println("Please choose one of the options showed below:");
         System.out.println("1. Add");
         System.out.println("2. Multiply");
@@ -52,14 +61,15 @@ public class Main {
 
     }
     public String EnterArguments(){
+        System.out.println("Welcome to the calculator");
         System.out.println("Please enter the numbers you need for the operation separated by a comma");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
 
     }
     public ArrayList<Integer> arrayListToInt(ArrayList<String> rawNumbers){
-        ArrayList<Integer> ints = new ArrayList<Integer>();
-        rawNumbers.stream().forEach(strNumb -> ints.add(Integer.parseInt(strNumb)));
+        ArrayList<Integer> ints = new ArrayList<>();
+        rawNumbers.forEach(strNumb -> ints.add(Integer.parseInt(strNumb)));
         return ints;
 
 
