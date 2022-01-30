1068678c111b0c842003fdbbe96a54c2a979f0c5
Rodrigo <rodri45ESP@gmail.com>
2022-01-30 11:13:18 +0100

    welcome message changed

--- Calculator/src/com/company/Main.java
+++ Calculator/src/com/company/Main.java
@@ -18,34 +18,36 @@ public class Main {
         ArrayList<Integer> intList = main.arrayListToInt(numberList);
         int option = main.ShowMenu();
 
-        if(option == 1) {
+        if (option == 1) {
             CalculateInterface sum = () -> {
-            Integer result = intList.stream()
-                    .mapToInt(Integer::intValue)
-                    .sum();
-            System.out.println(result);};
-        sum.Calculate();
-        }
-        else if(option == 2) {
+                Integer result = intList.stream()
+                        .mapToInt(Integer::intValue)
+                        .sum();
+                System.out.println(result);
+            };
+            sum.Calculate();
+        } else if (option == 2) {
             CalculateInterface multiplication = () -> {
-            int x;
-            x = intList.stream().reduce(1, (a, b) -> a * b);
-            System.out.println(x);};
+                int x;
+                x = intList.stream().reduce(0, (a, b) -> a * b);
+                System.out.println(x);
+            };
             multiplication.Calculate();
 
 
-        }
-        else if(option == 3) {
+        } else if (option == 3) {
             CalculateInterface substraction = () -> {
-            int x;
-            x = intList.stream().reduce(1, (a, b) -> a - b);
-            System.out.println(x);};
+                int x;
+                x = intList.stream().reduce(0, (a, b) -> a - b);
+                System.out.println(x);
+            };
             substraction.Calculate();
 
         }
         System.out.println("Done!");
     }
-    public int ShowMenu(){
+
+    public int ShowMenu() {
         Main main = new Main();
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please choose one of the options showed below:");
@@ -54,25 +56,25 @@ public class Main {
         System.out.println("3. Substract");
         int i;
         i = scanner.nextInt();
-        if(i <1 | i>3){
+        if (i < 1 | i > 3) {
             i = main.ShowMenu();
         }
         return i;
 
     }
-    public String EnterArguments(){
-        System.out.println("Welcome to the calculator");
+
+    public String EnterArguments() {
+        System.out.println("Welcome to the calculator!");
         System.out.println("Please enter the numbers you need for the operation separated by a comma");
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
 
     }
-    public ArrayList<Integer> arrayListToInt(ArrayList<String> rawNumbers){
+
+    public ArrayList<Integer> arrayListToInt(ArrayList<String> rawNumbers) {
         ArrayList<Integer> ints = new ArrayList<>();
         rawNumbers.forEach(strNumb -> ints.add(Integer.parseInt(strNumb)));
         return ints;
-
-
 
 
     }
